e91b9b1f3a7bc6a5d51bf193050515557207cac5
shuxin 　　zheng <zhengshuxin@gmail.com>
2020-04-16 16:08:32 +0800

    add one demo for gin(github.com/gin-gonic/gin)

---
+++ examples/gin-server/gin-server.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/sync/errgroup"
+	"github.com/acl-dev/master-go"
+)
+
+var (
+	g errgroup.Group	// Used to waiting for the service to be stopped.
+)
+
+var (
+	filePath    string	// the server's configure file.
+	listenAddrs string	// the server's listening addrs in alone mode.
+)
+
+// Start one fiber server for one listener which is a webserver.
+func startServer(listener net.Listener)  {
+	g.Go(func() error {
+		e := gin.New()
+		e.GET("/test", func(c *gin.Context) {
+			c.String(200, "hello world!\r\n")
+		})
+
+		log.Printf("Listen on %s", listener.Addr())
+		e.RunListener(listener)
+		return nil
+	})
+}
+
+func parseArgs()  {
+	flag.StringVar(&filePath, "c", "gin-server.cf", "Configure filePath")
+	flag.StringVar(&listenAddrs, "listen", "127.0.0.1:28880, 127.0.0.1:28881",
+		"Listen addr in alone running")
+
+	flag.Parse()
+}
+
+func main()  {
+	// parse args from commandline or acl_master's exec
+	parseArgs()
+
+	var listener []net.Listener
+	var err error
+
+	// Get all listeners which may be created by acl_master in daemon mode
+	// or created by the current process in alone mode; In daemon mode,
+	// the listeners' fds were created by acl_master and transfered to the
+	// children processes after fork/exec.
+
+	if master.Alone {
+		listener, err = master.ServiceInit(listenAddrs, nil)
+	} else {
+		listener, err = master.ServiceInit("", onStop)
+	}
+	if err != nil {
+		log.Printf("Listen error: %s\r\n", err)
+		return
+	}
+
+	// Start fibers for each listener which is listening on different addrs.
+	for _, ln := range listener {
+		startServer(ln)
+	}
+
+	log.Println("gin-server: Wait for services stop ...")
+
+	// Wait for the server to be stopped.
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("gin-server: All services stopped!\r\n")
+}
+
+// The callback used in master.ServiceInit() for the second arg, when the server
+// is to being stopped, the callback will be called.
+func onStop(ok bool) {
+	log.Println("gin-server: Disconnect from acl_master!")
+}
---
+++ examples/gin-server/start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./gin-server -alone -f gin-server.cf
